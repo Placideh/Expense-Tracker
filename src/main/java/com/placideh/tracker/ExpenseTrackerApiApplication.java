2376f47c82ecbfd8e069c608d974c6d518f0bd2b
Placideh <h.uwizeyeplacide@gmail.com>
2021-07-12 02:40:00 +0200

    added corsConfiguration for enable access to different ports server

--- src/main/java/com/placideh/tracker/ExpenseTrackerApiApplication.java
+++ src/main/java/com/placideh/tracker/ExpenseTrackerApiApplication.java
@@ -4,6 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 import com.placideh.tracker.filters.AuthFilter;
 
@@ -12,6 +16,19 @@ public class ExpenseTrackerApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExpenseTrackerApiApplication.class, args);
+	}
+	@Bean
+	public FilterRegistrationBean<CorsFilter> corsFilter(){
+		FilterRegistrationBean<CorsFilter>registrationBean=new FilterRegistrationBean<>();
+		UrlBasedCorsConfigurationSource source= new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config=new CorsConfiguration();
+		config.addAllowedOrigin("*");
+		config.addAllowedHeader("*");
+		source.registerCorsConfiguration("/**", config);
+		registrationBean.setFilter(new CorsFilter((CorsConfigurationSource) source));
+		registrationBean.setOrder(0);
+		return registrationBean;
+		
 	}
 	@Bean
 	public FilterRegistrationBean<AuthFilter> filterRegistrationBean(){
